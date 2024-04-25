cf7ce4c5119a279f851f689268e2870a9e3c160e
Jona <8698248+jonamil@users.noreply.github.com>
2024-04-25 15:28:36 +0200

    Add Tailwind theme config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,60 @@
 export default {
   content: ['./src/**/*.vue'],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ['"Source Sans 3 VF"', 'sans-serif'],
+      'serif-text': ['"Nice Text"', 'serif'],
+      'serif-micro': ['"Nice Micro"', 'serif'],
+    },
+    fontSize: {
+      base: ['1.03125rem', '1.125rem'],
+      'base-serif': ['1rem', '1.1875rem'],
+      small: '0.90625rem',
+    },
+    lineHeight: {
+      base: '1.125rem',
+      'paragraph-narrow': '1.3125rem',
+      'paragraph-wide': '1.375rem',
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      white: '#ffffff',
+      gray: {
+        50: '#F9F9FB',
+        100: '#EFF1F5', // header bg
+        200: '#DBDDE6', // borders
+        300: '#C7CAD6',
+        400: '#AFB3C0', // indentation
+        500: '#9598A4', // icons
+        600: '#82879C',
+        700: '#707794',
+        800: '#5C6484',
+        900: '#3E4560',
+        950: '#2A2F46', // text
+      },
+      orange: {
+        50: '#FFF9F5',
+        100: '#FFF3EB',
+        200: '#FFE3D1',
+        300: '#FFD4B8',
+        400: '#FFC299',
+        500: '#FFAF7A',
+        600: '#FF944D',
+        700: '#FF7519', // accent
+        800: '#E05A00',
+        900: '#9E3F00',
+        950: '#752F00',
+      },
+    },
+    extend: {
+      spacing: {
+        90: '22.5rem', // 360px
+      },
+      transitionDuration: {
+        200: '200ms',
+      },
+    },
   },
   plugins: [],
-}
-
+};

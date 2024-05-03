9a0a112b622467559ac841d81df9d316103bd9cd
Jona <8698248+jonamil@users.noreply.github.com>
2024-05-03 22:08:29 +0200

    Add placeholder API item

---
+++ src/stores/ContentStore.ts
@@ -0,0 +1,28 @@
+import { ref, computed } from 'vue';
+import { defineStore } from 'pinia';
+import type { HackerNewsItem } from '@/types';
+
+export const useContentStore = defineStore('content', () => {
+  const currentItem = ref<HackerNewsItem>({
+    by: 'scop',
+    id: 38571088,
+    descendants: 81,
+    kids: [
+      38571580, 38573214, 38573480, 38572058, 38572193, 38572010, 38571442, 38571862, 38580518,
+      38572927, 38575237, 38571465, 38571514, 38571718, 38572441, 38575227, 38571939, 38574618,
+      38573303, 38574532, 38573630, 38571630, 38590209, 38572917,
+    ],
+    score: 166,
+    time: 1702054197,
+    title: 'iA Writer in Paper',
+    type: 'story',
+    url: 'https://ia.net/topics/ia-writer-in-paper',
+  });
+
+  const currentUrl = computed(() => new URL(currentItem.value.url!));
+
+  return {
+    currentItem,
+    currentUrl,
+  };
+});
---
+++ src/types.ts
@@ -0,0 +1,25 @@
+export interface HackerNewsItem {
+  id: number;
+  deleted?: boolean;
+  type?: string;
+  by?: string;
+  time?: number;
+  text?: string;
+  dead?: boolean;
+  parent?: number;
+  poll?: number;
+  kids?: number[];
+  url?: string;
+  score?: number;
+  title?: string;
+  parts?: number[];
+  descendants?: number;
+}
+
+export interface HackerNewsUser {
+  id: string;
+  created: number;
+  karma: number;
+  about?: string;
+  submitted?: number[];
+}
